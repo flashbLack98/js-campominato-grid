7aa835fc62d309bf34499772fb557fb4538b5f59
flashbLack98 <npalakuttige98@icloud.com>
2021-10-19 22:25:12 +0200

    complete

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 
-
+//CREARE LA VARIABILE DEL NUMERO DI CELLE
+//DA INSERIRE A RIGA 96 102 E 103
 
 
 const selectDifficolta = document.getElementById("livello");
@@ -14,7 +15,7 @@ const containerUniversale = document.querySelector(".container");
 const btnPlay = document.getElementById("btn_play");
 
 /**  
- *   
+ *   funzione che determina la larghezza/altezza di un box
  * * @param x=lato del quadrato(box)
  * * @param y=numero di celle
  * */
@@ -25,7 +26,7 @@ function sizeBox(y) {
 console.log(sizeBox(49));
 
 /* ------------------------------------------------------------ */
-/* function generazioneGriglia(nBox,) {
+function generazioneGriglia(nBox) {
 
     //CREARE IL DIV DELLA GRIGLIA
     const grillContainer = document.createElement("div");
@@ -36,6 +37,7 @@ console.log(sizeBox(49));
 
     //INSERIRE LA GRIGLIA NEL CONTAINER UNIVERSALE
     containerUniversale.append(grillContainer);
+
 
 
     //CREARE UNA CELLA DENTRO LA GRIGLIA
@@ -45,21 +47,23 @@ console.log(sizeBox(49));
 
         //DARE GLI STILI ALLA CELLA DELLA GRIGLIA
         box.classList.toggle("box");
-
+        box.style.height = "calc(100% / " + sizeBox(nBox);
+        box.style.width = "calc(100% / " + sizeBox(nBox);
         //INSERIRE LA CELLA NELLA GRIGLIA
         grillContainer.append(box);
+
+        //INSERIRE IN NUMERI DENTRO OGNI BOX
+        box.textContent = i + 1;
 
         //QUANDO SCHIACCIO UN BOTTONE QUESTO DEVE CAMBIARE COLORE
         box.addEventListener("click", function () {
             this.classList.toggle("box_on");
         });
     }
-} */
-
+}
+//------------------------------------------------------------
 //QUANDO SCHIACCIO IL BOTTONE PLAY DEVO:
 btnPlay.addEventListener("click", function () {
-
-
 
 
     //CAPIRE CHE LIVELLO DI DIFFICOLTA' HO SCELTO
@@ -68,49 +72,15 @@ btnPlay.addEventListener("click", function () {
 
     switch (difficolta) {
         case "1":
-
+            generazioneGriglia(100);
             break;
         case "2":
-
+            generazioneGriglia(81);
             break;
         case "3":
-
+            generazioneGriglia(49);
             break;
     }
-
-
-    //CREARE IL DIV DELLA GRIGLIA
-    const grillContainer = document.createElement("div");
-    console.log(grillContainer);
-
-    //DARE GLI STILI ALLLA GRIGLIA
-    grillContainer.classList.toggle("grill_container");
-
-    //INSERIRE LA GRIGLIA NEL CONTAINER UNIVERSALE
-    containerUniversale.append(grillContainer);
-
-
-
-    //CREARE UNA CELLA DENTRO LA GRIGLIA
-    for (i = 0; i < 100; i++) {
-
-        const box = document.createElement("div");
-
-        //DARE GLI STILI ALLA CELLA DELLA GRIGLIA
-        box.classList.toggle("box");
-        box.style.height = "calc(100% / " + sizeBox(100);
-        box.style.width = "calc(100% / " + sizeBox(100);
-        //INSERIRE LA CELLA NELLA GRIGLIA
-        grillContainer.append(box);
-
-        //QUANDO SCHIACCIO UN BOTTONE QUESTO DEVE CAMBIARE COLORE
-        box.addEventListener("click", function () {
-            this.classList.toggle("box_on");
-        });
-    }
-
-
-
 
 
 
